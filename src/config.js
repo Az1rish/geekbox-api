168f714a061fd7b886f600cb248f6d47cd59423e
Shane <shanemcneil19@gmail.com>
2020-03-14 19:36:34 -0700

    add API_BASE_URL to config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: 'https://geekbox.now.sh/',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://geekbox:geekbox@localhost/geekbox',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://geekbox:geekbox@localhost/geekbox-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://geekbox:geekbox@localhost/geekbox-test',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'
   }
