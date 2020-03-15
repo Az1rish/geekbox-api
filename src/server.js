9cb58b13b628371d51fa46768c24af4b4e69f151
Shane <shanemcneil19@gmail.com>
2020-03-14 19:31:17 -0700

    set up server file with knex and require app

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const app = express();
+const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config')
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
 });
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
-
-module.exports = {app};
