976dc2ed40b2ef073ec66c3130b6594d09bbb9bb
Shane <shanemcneil19@gmail.com>
2020-03-14 19:37:35 -0700

    add JWT_SECRET to config

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ module.exports = {
     CLIENT_ORIGIN: 'https://geekbox.now.sh/',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://geekbox:geekbox@localhost/geekbox',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://geekbox:geekbox@localhost/geekbox-test',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api',
+    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
   }
