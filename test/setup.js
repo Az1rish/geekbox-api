801d636aff0abf137b01b3d8e2f673056387d900
Shane <shanemcneil19@gmail.com>
2020-03-14 19:59:13 -0700

    write set up file for tests

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,12 @@
-const { expect } = require('chai')
-const supertest = require('supertest')
+process.env.TZ = 'UTC';
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test-jwt-secret';
 
-global.expect = expect
-global.supertest = supertest
+require('dotenv').config();
+const { expect } = require('chai');
+const supertest = require('supertest');
+
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "postgresql://geekbox:geekbox@localhost/geekbox-test";
+
+global.expect = expect;
+global.supertest = supertest;
