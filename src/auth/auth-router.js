5ccbb06b9f8e513ef89803f71d4658658b5eccea
Shane <shanemcneil19@gmail.com>
2020-03-15 15:06:20 -0700

    write auth router

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -16,4 +16,39 @@ authRouter
                 });
             }
         }
-    })
+
+        AuthService.getUserWithUserName(
+            req.app.get('db'),
+            signinUser.user_name
+        )
+            .then((dbUser) => {
+                if (!dbUser) {
+                    return res.status(400).json({
+                        error: 'Incorrect user_name or password'
+                    });
+                }
+
+                return AuthService.comparePasswords(signinUser.password, dbUser.password)
+                    .then((compareMatch) => {
+                        if (!compareMatch) {
+                            return res.status(400).json({
+                                error: 'Incorrect user_name or password'
+                            });
+                        }
+
+                        const sub = dbUser.user_name;
+                        const payload = {
+                            user_id: dbUser.id
+                        };
+                        res.send({
+                            authToken: AuthService.createJwt(sub, payload)
+                        });
+                    });
+            })
+            .then((user) => {
+                res.json(user);
+            })
+            .catch(next);
+    });
+
+module.exports = authRouter;
