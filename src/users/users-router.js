cf46c91cae57d84d07931e801edafb3e915198fd
Shane <shanemcneil19@gmail.com>
2020-03-28 13:23:23 -0700

    debug users post request

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -14,6 +14,7 @@ usersRouter
             .catch(next);
     })
     .post('/', jsonBodyParser, (req, res, next) => {
+        console.log('REQ', req)
         const { password, user_name, first_name, last_name } = req.body;
 
         for (const field of ['first_name', 'last_name', 'user_name', 'password']) {
